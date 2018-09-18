fbe3e4e3fda8234f09d10ff2e2e1c91eeec43188
Javier Álvarez <javier.alvnu@gmail.com>
2018-09-18 17:56:04 +0200

    Filters easier to change; added option to get player secondary position

--- tranfermarkt.py
+++ tranfermarkt.py
@@ -3,7 +3,7 @@ from timer import Timer
 from webscraper import WebScraper as WebScr
 
 
-class TransferMarket:
+class TransferMarkt:
 
     def __init__(self):
         WebScr.base_url = "https://www.transfermarkt.es"
@@ -13,14 +13,14 @@ class TransferMarket:
     def get_rows_from_link(url):
         page = WebScr.get_soup(url)
         if page:
-            return TransferMarket.get_rows_from_page(page)
+            return TransferMarkt.get_rows_from_page(page)
 
     @staticmethod
     def get_rows_from_page(page):
         table = page.find('table', {'class': 'items'})
         return table.tbody.find_all('tr', recursive=False)
 
-    def get_leagues(self):
+    def get_leagues(self, min_value=80000, force_include=None):
         leagues = {}
         total_teams = 0
         first_tier = True
@@ -31,7 +31,10 @@ class TransferMarket:
                 for row in rows:
                     if 'colspan' not in row.td.attrs:
                         res = self.get_league_info(row)
-                        if res and res['value'] > 80000 or res['country'] == "España":
+                        if res and (
+                                res['value'] > min_value or
+                                res['id'] in force_include
+                        ):
                             leagues[res['id']] = {}
                             leagues[res['id']]['name'] = res['name']
                             leagues[res['id']]['link'] = res['link']
@@ -76,7 +79,7 @@ class TransferMarket:
         }
         return league
 
-    def get_teams(self, leagues):
+    def get_teams(self, leagues, extra_player_positions=False):
         for id_league in leagues:
             teams = {}
             rows = self.get_rows_from_link(leagues[id_league]['link'])
@@ -87,7 +90,10 @@ class TransferMarket:
                 page = WebScr.get_soup(link)
                 if page:
                     name = page.find('h1', {'itemprop': 'name'}).span.text
-                    players = TransferMarket.get_players(TransferMarket.get_rows_from_page(page))
+                    players = TransferMarkt.get_players(TransferMarkt.get_rows_from_page(page))
+                    if extra_player_positions:
+                        for id_player in players:
+                            players[id_player] = TransferMarkt.get_extra_player_position(players[id_player])
                     teams[id_team] = {'link': link, 'name': name, 'players': players}
                     self.timer.add_done()
                     self.timer.print_left()
@@ -115,7 +121,7 @@ class TransferMarket:
                 'dorsal': aux_0 if (len(aux_0) > 1) else '',
                 'link': aux_1a['href'],
                 'name': aux_1a['title'],
-                'birth': TransferMarket.format_birth_date(aux_2) if ('/' in aux_2) else '',
+                'birth': TransferMarkt.format_birth_date(aux_2) if ('/' in aux_2) else '',
                 'nationality': data[3].img['title'],
                 'height': aux_4 if (len(aux_4) > 1) else '',
                 'foot': aux_5 if (len(aux_5) > 1) else '',
@@ -125,6 +131,19 @@ class TransferMarket:
         return players
 
     @staticmethod
+    def get_extra_player_position(player):
+        page = WebScr.get_soup(player['link'])
+        div = page.find('div', {'class': 'nebenpositionen'})
+        if div:
+            txt = div.text
+            position_2 = txt[txt.find(':') + 1:].strip()
+            if len(position_2) > 25:
+                position_2 = position_2[:25].strip()
+            player['position_2'] = position_2
+        print(player)
+        return player
+
+    @staticmethod
     def format_birth_date(date):
         aux = date.split(' ')[0]
         var = aux.split('/')
@@ -132,13 +151,20 @@ class TransferMarket:
 
 
 if __name__ == "__main__":
-    tm = TransferMarket()
+    force_include = [
+        'GB1', 'L1', 'IT1', 'FR1', 'RU1', 'NL1', 'PO1', 'AZ1', 'KR1', 'RO1', 'SE1', 'SER1', 'WER1'
+        'GB2', 'L2', 'IT2', 'FR2', 'RU2', 'NL2', 'PO2',
+        'GB3', 'L3',
+        'ES1', 'ES2', 'ES3A', 'ES3B', 'ES3C', 'ES3D',
+    ]
+
+    tm = TransferMarkt()
     print("Obtaining leagues...")
-    leagues = tm.get_leagues()
+    leagues = tm.get_leagues(80000, force_include)
     print("Leagues obtained!")
 
     print("Getting team and player data from each league...")
-    teams = tm.get_teams(leagues)
+    teams = tm.get_teams(leagues, extra_player_positions=False)
     print("All info downloaded, saving it...")
     with open('output.json', 'w') as f:
         json.dump(leagues, f, indent=4, sort_keys=True)
